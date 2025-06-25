e56e0b0792dd15bc31ba5e645d4ea123703dd545
MohammedBakri1503 <mohammedbakri1503@gmail.com>
2025-06-25 19:01:15 +0300

    modify conftest

--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
 import json
 import pytest
+from api.posts_api import PostsAPI
 
 @pytest.fixture
-def valid_payload():
+def posts_api():
+    return PostsAPI()
+
+@pytest.fixture
+def valid_post_data():
     with open("data/post_payloads.json") as f:
         return json.load(f)["valid_post"]
 
 @pytest.fixture
-def invalid_payload():
+def invalid_post_data():
     with open("data/post_payloads.json") as f:
         return json.load(f)["invalid_post"]
